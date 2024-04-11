cc256f00a9baaafac884b406942e31a03fae2ca3
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-04-11 14:52:06 +0530

    'use client';

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import AddToCard from './AddToCart'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <div>
+
+ <AddToCart />
+
+
+    </div>
+  )
+}
+
+export default ProductCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
 
 <h1>Hello Next JS</h1>
  <Link href='/users'> Users</Link>
+ <ProductCard />
 
    </main>
   )
