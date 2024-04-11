c8a97567e19d5487fd38d64f179c9262040ec981
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-04-11 19:07:12 +0530

    Caching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
 
-const UsersPage = () => {
-  return (
-    <div>UsersPage
 
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+const users: User[] = await res.json()
+
+//caching
+
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users', 
+// {cache: 'no-store'}) //this is to disable caching
+
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users',
+// {next: {revalidate:10}})// revalidate every 10 seconds
+
+  return (
+    <>
+
+  <h1>Users</h1>
+
+  <ul>
+
+  {users.map(user => <li key={user.id}>{user.name}</li>)}
+  
+  </ul>
   
 
-    </div>
+    </>
 
  
   )
